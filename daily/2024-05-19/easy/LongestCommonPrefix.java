0ee22abb873a6b2595de870038b1997fc6fcf35e
Sai Kumar Reddy <saikumarreddy1101@gmail.com>
2024-05-19 12:00:00 -0700

    Practice (2024-05-19): LongestCommonPrefix

---
+++ daily/2024-05-19/easy/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+/**
+ * Problem: Longest Common Prefix
+ * Link:    https://leetcode.com/problems/longest-common-prefix/
+ * Level:   Easy
+ * Topic:   Strings
+ *
+ * Approach: Use first string as reference; shorten while others don't start with it.
+ * Time:  O(n*m)
+ * Space: O(1)
+ */
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (String s : strs) {
+            while (!s.startsWith(prefix)) prefix = prefix.substring(0, prefix.length()-1);
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix sol = new LongestCommonPrefix();
+        System.out.println(sol.longestCommonPrefix(new String[]{"flower","flow","flight"})); // fl
+        System.out.println(sol.longestCommonPrefix(new String[]{"dog","racecar","car"}));    // (empty)
+    }
+}
