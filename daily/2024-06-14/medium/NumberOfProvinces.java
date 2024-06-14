9c1354be1c25a3c0476c4bf6e56ef0ddfff54f69
Sai Kumar Reddy <saikumarreddy1101@gmail.com>
2024-06-14 12:00:00 -0700

    Practice (2024-06-14): NumberOfProvinces

---
+++ daily/2024-06-14/medium/NumberOfProvinces.java
@@ -0,0 +1,30 @@
+/**
+ * Problem: Number of Provinces
+ * Link:    https://leetcode.com/problems/number-of-provinces/
+ * Level:   Medium
+ * Topic:   Graphs, DFS, Union Find
+ *
+ * Approach: DFS from each unvisited city; count connected components.
+ * Time:  O(n^2)
+ * Space: O(n)
+ */
+
+public class NumberOfProvinces {
+
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length, count=0;
+        boolean[] visited=new boolean[n];
+        for (int i=0;i<n;i++) if(!visited[i]) { dfs(isConnected,visited,i); count++; }
+        return count;
+    }
+    private void dfs(int[][] g, boolean[] vis, int i) {
+        vis[i]=true;
+        for (int j=0;j<g.length;j++) if(!vis[j]&&g[i][j]==1) dfs(g,vis,j);
+    }
+
+    public static void main(String[] args) {
+        NumberOfProvinces sol = new NumberOfProvinces();
+        System.out.println(sol.findCircleNum(new int[][]{{1,1,0},{1,1,0},{0,0,1}})); // 2
+        System.out.println(sol.findCircleNum(new int[][]{{1,0,0},{0,1,0},{0,0,1}})); // 3
+    }
+}
