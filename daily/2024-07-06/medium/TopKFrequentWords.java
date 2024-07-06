9e9ae91afd6f5c3e89b39a2aa1a2e9cf1a72455e
Sai Kumar Reddy <saikumarreddy1101@gmail.com>
2024-07-06 12:00:00 -0700

    Practice (2024-07-06): TopKFrequentWords

---
+++ daily/2024-07-06/medium/TopKFrequentWords.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Top K Frequent Elements
+ * Link:    https://leetcode.com/problems/top-k-frequent-elements/
+ * Level:   Medium
+ * Topic:   Heap, Hashing
+ *
+ * Approach: Count frequencies, then use min-heap of size k to track top-k.
+ * Time:  O(n log k)
+ * Space: O(n)
+ */
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class TopKFrequentWords {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int n:nums) freq.merge(n,1,Integer::sum);
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->freq.get(a)-freq.get(b));
+        for(int n:freq.keySet()) { pq.offer(n); if(pq.size()>k) pq.poll(); }
+        int[] res=new int[k];
+        for(int i=k-1;i>=0;i--) res[i]=pq.poll();
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TopKFrequentWords sol = new TopKFrequentWords();
+        System.out.println(Arrays.toString(sol.topKFrequent(new int[]{1,1,1,2,2,3},2))); // [1,2]
+    }
+}
