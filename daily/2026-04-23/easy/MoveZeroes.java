d853ac5af500407b88a9c4d98a10f9fdf9b0356e
Sai Kumar Reddy <saikumarreddy1101@gmail.com>
2026-04-23 12:00:00 -0700

    Practice (2026-04-23): MoveZeroes

---
+++ daily/2026-04-23/easy/MoveZeroes.java
@@ -0,0 +1,31 @@
+/**
+ * Problem: Move Zeroes
+ * Link:    https://leetcode.com/problems/move-zeroes/
+ * Level:   Easy
+ * Topic:   Arrays, Two Pointers
+ *
+ * Approach: Left pointer tracks insertion spot; swap non-zero elements forward.
+ * Time:  O(n)
+ * Space: O(1)
+ */
+import java.util.Arrays;
+
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int tmp = nums[left]; nums[left] = nums[right]; nums[right] = tmp;
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes sol = new MoveZeroes();
+        int[] a = {0,1,0,3,12};
+        sol.moveZeroes(a);
+        System.out.println(Arrays.toString(a)); // [1, 3, 12, 0, 0]
+    }
+}
